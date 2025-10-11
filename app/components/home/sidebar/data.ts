361707aa1d1972128ef2808c3429388acdd040f8
amaniCoding <amanuelfrm@gmail.com>
2025-10-11 11:13:17 +0300

    incase

---
+++ app/components/home/sidebar/data.ts
@@ -0,0 +1,75 @@
+export const data = [
+  {
+    name: "friends",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Memories",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Saved",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Groups",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Video",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Marketplace",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Feeds",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Events",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Ads Manager",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Play Games",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Birth Days",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Gaming Video",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Messenger",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Messenger Kids",
+    icon: "/sidebar/friend.png",
+  },
+
+  {
+    name: "Order and payments",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Pages",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Recent Ad Activity",
+    icon: "/sidebar/friend.png",
+  },
+  {
+    name: "Ads Manager",
+    icon: "/sidebar/friend.png",
+  },
+];
