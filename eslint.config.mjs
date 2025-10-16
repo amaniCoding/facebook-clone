cb54b7312491244a28091c3603beb71bb0c57a0b
amaniCoding <amanuelfrm@gmail.com>
2025-10-16 13:15:53 +0300

    incase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
   }),
 
   {
