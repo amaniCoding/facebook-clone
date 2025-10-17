65e055d4c4a941c0cc9c4cd1eea86c31cbe44462
amaniCoding <amanuelfrm@gmail.com>
2025-10-17 12:53:32 +0300

    new

--- app/libs/prisma.ts
+++ app/libs/prisma.ts
@@ -1,7 +1,14 @@
+// lib/prisma.ts
+
 import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
