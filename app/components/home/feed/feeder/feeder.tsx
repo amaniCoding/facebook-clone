dd6e11f678bfead5217a44b0ba3d9421534812b7
amaniCoding <amanuelfrm@gmail.com>
2025-12-06 11:57:18 +0300

    UI changed

--- app/components/home/feed/feeder/feeder.tsx
+++ app/components/home/feed/feeder/feeder.tsx
@@ -1,17 +1,19 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import Post from "../post/post";
 import CommentModal from "../commentmodal/comment-modal";
-import { useAppSelector } from "@/app/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 
 import ReactionModal from "../reactionmodal/reactionmodal";
 import useSWRInfinite from "swr/infinite";
 import { FeedsType } from "@/app/api/feeder/[page]/lib";
 import FeedItemSkeleton from "@/app/components/skeletons/feed";
+import { setFeeds } from "@/app/store/slices/feed/feed";
 interface FeedsPage {
   feeds: FeedsType[];
 }
 export default function Feeder() {
+  const dispatch = useAppDispatch();
   const isCommentModalOpen = useAppSelector(
     (state) => state.commentModal.isOpen
   );
@@ -19,6 +21,7 @@ export default function Feeder() {
   const isReactionModalOpen = useAppSelector(
     (state) => state.reactionModal.isOpen
   );
+  const feedsState = useAppSelector((state) => state.feed.feeds.feeds);
 
   const fetcher = async (url: string): Promise<FeedsPage> => {
     const res = await fetch(url);
@@ -38,11 +41,9 @@ export default function Feeder() {
   const { data, error, size, setSize, isLoading, isValidating } =
     useSWRInfinite<FeedsPage>(getKey, fetcher);
 
-  // const feeds: FeedsType[] = useMemo(() => {
-  //   return data ? data.flatMap((page) => page.feeds) : [];
-  // }, [data]);
-
-  const feeds = data ? data.flatMap((page) => page.feeds) : [];
+  const feeds: FeedsType[] = useMemo(() => {
+    return data ? data.flatMap((page) => page.feeds) : [];
+  }, [data]);
 
   const observerRef = useRef<HTMLDivElement>(null);
 
@@ -50,6 +51,14 @@ export default function Feeder() {
     isLoading ||
     (isValidating && data && typeof data[size - 1] === "undefined");
   const isReachingEnd = data && data[data.length - 1]?.feeds.length < PAGE_SIZE;
+
+  useEffect(() => {
+    dispatch(
+      setFeeds({
+        feeds: feeds,
+      })
+    );
+  }, [dispatch, feeds]);
 
   useEffect(() => {
     if (isReachingEnd || !observerRef.current) return;
@@ -69,12 +78,13 @@ export default function Feeder() {
   }, [size, setSize, isReachingEnd, isLoadingMore]);
 
   if (error) return <div>Failed to load posts</div>;
-  if (isLoading) return <FeedItemSkeleton />;
   return (
     <>
-      {feeds!.map((post, index) => (
+      {feedsState!.map((post, index) => (
         <Post key={index} post={post} />
       ))}
+
+      <div ref={observerRef}>{isLoading && <FeedItemSkeleton />}</div>
 
       {isCommentModalOpen && <CommentModal />}
       {isReactionModalOpen && <ReactionModal />}
